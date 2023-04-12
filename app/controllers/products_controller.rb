8c6fabd6f0c96de2174d5423d10e87adf599b1e3
Knyazik <viktor.kn@icloud.com>
2023-04-12 20:28:09 +0300

    change user product selection (to buy and bought)

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,18 +2,13 @@ class ProductsController < ApplicationController
   before_action :authorize_request
 
   def users_bought
-    products_user_bought = ProductService.get_products_user_bought(@current_user.id)
+    products_user_bought = ProductService.get_products_user_bought(@current_user)
     render json: products_user_bought.to_json(include: :currency), status: :ok
   end
 
   def user_can_buy
     # Fetch all products that do not have a purchase associated with a specific user_id and product_id
-    products_can_buy = Product
-                         .left_outer_joins(:purchases)
-                         .where.not(
-      id: Purchase.select(:product_id).where(user_id: @current_user.id)
-    )
-                         .order(:name)
+    products_can_buy = ProductService.get_products_user_can_buy(@current_user)
 
     render json: products_can_buy.to_json(include: :currency), status: :ok
   end
@@ -32,13 +27,13 @@ class ProductsController < ApplicationController
         if File.exist?(file_path)
           send_file(file_path, disposition: 'attachment', filename: file_name)
         else
-          render json: {error: 'File not found'}, status: :not_found
+          render json: { error: 'File not found' }, status: :not_found
         end
       else
-        render json: {error: 'Product not found'}, status: :not_found
+        render json: { error: 'Product not found' }, status: :not_found
       end
     else
-      render json: {error: 'You cannot download this file'}, status: :forbidden
+      render json: { error: 'You cannot download this file' }, status: :forbidden
     end
   end
 
--- app/services/product_service.rb
+++ app/services/product_service.rb
@@ -1,8 +1,12 @@
 class ProductService
-  def self.get_products_user_bought(user_id)
-    Product
-      .left_outer_joins(:purchases)
-      .where(purchases: { user_id: user_id })
+  def self.get_products_user_bought(user)
+    purchased_product_ids = Purchase.where(user: user).pluck(:product_id)
+    Product.where(id: purchased_product_ids).order(:id)
+  end
+
+  def self.get_products_user_can_buy(user)
+    purchased_product_ids = Purchase.where(user: user).pluck(:product_id)
+    Product.where.not(id: purchased_product_ids).order(:id)
   end
 
   def self.is_user_bought(product_id, user)
